f4703d72936b37548cb98cfdc8edb4215b110aef
Ibtehaz <10481417+shepherd-06@users.noreply.github.com>
2023-02-02 11:34:33 +0200

    removed default contents, added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      journey: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {/* nav bar */}
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+          <a className="navbar-brand" href="#">Home</a>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home
+                  <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        {/* ----- */}
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
